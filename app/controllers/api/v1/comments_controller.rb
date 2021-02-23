70525166e59f06e843fbeb5aaa7c88e065d74948
github username <jhernandez1692@gmail.com>
2021-02-23 12:15:12 -0500

    comments controller.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index
+        comments = Comment.all
+        render json: CommentSerializer.new(comments)
+    end
+
+    def create
+        comment = Comment.new(comment_params)
+  
+        if comment.save
+            render json: CommentSerializer.new(comment), status: :accepted
+        else
+            render json: {errors: comment.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        comment = Comment.find_by(id: params[:id])
+        binding.pry
+        if comment.update(comment_params)
+          render json: CommentSerializer.new(comment), status: :accepted
+        else
+            render json: {errors: comment.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :discussion_id)
+    end
+
+    
 end
+
