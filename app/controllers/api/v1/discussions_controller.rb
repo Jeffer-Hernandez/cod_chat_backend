f2675db0a56ec937a268b58442fcc6bf4ddacac4
github username <jhernandez1692@gmail.com>
2021-02-23 12:15:00 -0500

    discussion controller.

--- app/controllers/api/v1/discussions_controller.rb
+++ app/controllers/api/v1/discussions_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::DiscussionsController < ApplicationController
+
+    def index
+        discussion = Discussion.all
+        render json: DiscussionSerializer.new(discussion)
+    end
+
+    def create
+        discussion = Discussion.new(discussion_params)
+
+        if discussion.save
+            render json: discussion, status: :accepted
+        else
+            render json: {errors: discussion.errors.full_messages}, status: :unprocessable_entity
+        end
+        
+    end
+
+    private
+
+    def discussion_params
+        params.require(:discussion).permit(:title)
+    end
+
 end
