d83c878a617e7b821cc2378fbcbb1635ccffa170
github username <jhernandez1692@gmail.com>
2021-02-24 16:31:23 -0500

    Implement Delete action.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -17,12 +17,19 @@ class Api::V1::CommentsController < ApplicationController
 
     def update
         comment = Comment.find_by(id: params[:id])
-        binding.pry
+ 
         if comment.update(comment_params)
           render json: CommentSerializer.new(comment), status: :accepted
         else
             render json: {errors: comment.errors.full_messages}, status: :unprocessable_entity
         end
+    end
+
+    def destroy
+        comment = Comment.find_by(id: params[:id]).destroy
+
+        render json: CommentSerializer.new(comment), status: :accepted
+
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :discussions, only: [:index, :create]
-      resources :comments, only: [:index, :create, :update]
+      resources :discussions
+      resources :comments
     end
   end
 end
